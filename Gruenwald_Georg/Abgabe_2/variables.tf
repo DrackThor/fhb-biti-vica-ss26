39908644b644ffb3c43b5097dd1ccd7a986b6fc4
gmgruenwald <georg.m.gruenwald@gmail.com>
2026-05-10 23:50:56 +0200

    refactor(variables): transition to prefix-based domain configuration

--- Gruenwald_Georg/Abgabe_2/variables.tf
+++ Gruenwald_Georg/Abgabe_2/variables.tf
@@ -3,6 +3,6 @@ variable "exoscale_api_secret" { sensitive = true }
 
 variable "zone" { default = "at-vie-1" }
 variable "root_domain" { description = "The base domain (e.g., example.com" }
-variable "stats_domain" { description = "Subdomain for Netdata" }
-variable "api_domain" { description = "Subdomain for Swagger UI" }
+variable "stats_prefix" { description = "Subdomain for Netdata" }
+variable "api_prefix" { description = "Subdomain for Swagger UI" }
 variable "admin_email" { description = "Email for Let's Encrypt" }
