ba90a0cfd9e63c4f208edcc097b44575097d2329
gmgruenwald <georg.m.gruenwald@gmail.com>
2026-05-11 23:09:21 +0200

    fix(terraform): resolve variable naming and block syntax

--- Gruenwald_Georg/Abgabe_2/variables.tf
+++ Gruenwald_Georg/Abgabe_2/variables.tf
@@ -1,8 +1,43 @@
-variable "exoscale_api_key" { sensitive = true }
-variable "exoscale_api_secret" { sensitive = true }
+variable "exoscale_api_key" {
+  type      = string
+  sensitive = true
+}
 
-variable "zone" { default = "at-vie-1" }
-variable "root_domain" { description = "The base domain (e.g., example.com" }
-variable "stats_prefix" { description = "Subdomain for Netdata" }
-variable "api_prefix" { description = "Subdomain for Swagger UI" }
-variable "admin_email" { description = "Email for Let's Encrypt" }
+variable "exoscale_api_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "zone" {
+  type    = string
+  default = "at-vie-1"
+}
+
+variable "vm_name" {
+  type    = string
+  default = "vm-ggruenwald"
+}
+
+variable "root_domain" {
+  type        = string
+  default     = "biti-fhb.org"
+  description = "The base domain (e.g., example.com)"
+}
+
+variable "second_level_domain" {
+  type        = string
+  default     = "ggruenwald"
+  description = "2LD for sharing base domain"
+}
+
+variable "stats_prefix" {
+  type        = string
+  default     = "stats"
+  description = "Subdomain for Netdata"
+}
+
+variable "api_prefix" {
+  type        = string
+  default     = "api"
+  description = "Subdomain for Swagger UI"
+}
