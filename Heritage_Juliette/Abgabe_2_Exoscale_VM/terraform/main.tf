00cf0e7d0a758a12d8ba264d9f37cf97a438e7c9
heritagej <julietteheritage@yahoo.com>
2026-05-14 11:45:29 +0200

    add exoscale vm resource

--- Heritage_Juliette/Abgabe_2_Exoscale_VM/terraform/main.tf
+++ Heritage_Juliette/Abgabe_2_Exoscale_VM/terraform/main.tf
@@ -49,5 +49,31 @@ resource "exoscale_security_group_rule" "ssh_ingress" {
   start_port = 22
   end_port   = 22
 
+# Exoscale VM erstellen.
+resource "exoscale_compute_instance" "vm" {
+
+  # Zone der VM.
+  zone = var.zone
+
+  # Name der VM.
+  name = var.instance_name
+
+  # Ubuntu Template.
+  template = var.instance_template
+
+  # VM-Größe.
+  type = var.instance_type
+
+  # Root-Disk-Größe.
+  disk_size = 20
+
+  # Öffentliche IP automatisch zuweisen.
+  ipv6 = false
+
+  # Security Group zuweisen.
+  security_group_ids = [
+    exoscale_security_group.vm_sg.id
+  ]
+}
   cidr = "0.0.0.0/0"
 }
