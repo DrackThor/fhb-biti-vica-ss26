a3ce3e2766b696e9c167a7c2569b26298bac85bc
djov-artifact <cipherchroniclex@gmail.com>
2026-06-07 19:28:52 +0200

    feat: Exoscale Terraform setup und Github Actions pipelines neu

---
+++ Jovic_David/Abgabe_2_260607/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    exoscale = {
+      source  = "exoscale/exoscale"
+      version = "~> 0.59.0"
+    }
+  }
+  backend "s3" {
+    bucket                      = "djov-tfstate"
+    key                         = "production/terraform.tfstate"
+    region                      = "at-vie-1"
+    endpoint                    = "https://sos-at-vie-1.exo.io"
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_metadata_api_check     = true
+  }
+}
+
+
+provider "exoscale" {
+  key    = var.exoscale_key
+  secret = var.exoscale_secret
+}
